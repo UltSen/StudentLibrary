413a8fcbddb0a2f39c681681c74c7f02dbd2a007
CobainCupido <cobaincupido@gmail.com>
2022-10-21 17:47:37 +0200

    I setup the classes *1st I'm going to need this projects name to be renamed to LibraryServer and another project named LibraryClient so I can commit the client side so we might need a second  resporitory *2nd Ill send the Database via email but that will come through tomorrow

---
+++ src/main/java/za/ac/cput/LibraryClient.java
@@ -0,0 +1,9 @@
+package za.ac.cput;
+/*
+* StudentNo:217068324
+* Name: Cobain Liam Cupido
+* Class Function: 
+*/
+public class LibraryClient {
+    
+}
---
+++ src/main/java/za/ac/cput/views/ReturnBookGUI.java
@@ -0,0 +1,9 @@
+package za.ac.cput.views;
+/*
+* StudentNo:217068324
+* Name: Cobain Liam Cupido
+* Class Function: 
+*/
+public class ReturnBookGUI {
+    
+}
