35a6617bc63a4c57acf500ddac10a464ca3c4a85
CobainCupido <cobaincupido@gmail.com>
2022-10-22 15:22:00 +0200

    Edited the Book and leaner classes to add the data types we will be using but if i missed anything edit and get back to me

--- src/main/java/za/ac/cput/domain/Book.java
+++ src/main/java/za/ac/cput/domain/Book.java
@@ -5,5 +5,67 @@ package za.ac.cput.domain;
 * Class Function: 
 */
 public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private String catagory;
+    private String shelfNo;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String title, String author, String catagory, String shelfNo) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.catagory = catagory;
+        this.shelfNo = shelfNo;
+    }
+    
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCatagory() {
+        return catagory;
+    }
+
+    public void setCatagory(String catagory) {
+        this.catagory = catagory;
+    }
+
+    public String getShelfNo() {
+        return shelfNo;
+    }
+
+    public void setShelfNo(String shelfNo) {
+        this.shelfNo = shelfNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "isbn=" + isbn + ", title=" + title + ", author=" + author + ", catagory=" + catagory + ", shelfNo=" + shelfNo + '}';
+    }
     
 }
--- src/main/java/za/ac/cput/domain/Learner.java
+++ src/main/java/za/ac/cput/domain/Learner.java
@@ -6,4 +6,68 @@ package za.ac.cput.domain;
 */
 public class Learner {
     
+    private String studendNo;
+    private String firstName;
+    private String secondName;
+    private String classNo;
+    private boolean borrowedBook;  
+
+    public Learner() {
+    }
+
+    public Learner(String studendNo, String firstName, String secondName, String classNo, boolean borrowedBook) {
+        this.studendNo = studendNo;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.classNo = classNo;
+        this.borrowedBook = borrowedBook;
+    }
+
+    public String getStudendNo() {
+        return studendNo;
+    }
+
+    public void setStudendNo(String studendNo) {
+        this.studendNo = studendNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getClassNo() {
+        return classNo;
+    }
+
+    public void setClassNo(String classNo) {
+        this.classNo = classNo;
+    }
+
+    public boolean isBorrowedBook() {
+        return borrowedBook;
+    }
+
+    public void setBorrowedBook(boolean borrowedBook) {
+        this.borrowedBook = borrowedBook;
+    }
+
+    @Override
+    public String toString() {
+        return "Learner{" + "studendNo=" + studendNo + ", firstName=" + firstName + ", secondName=" + secondName + ", classNo=" + classNo + ", borrowedBook=" + borrowedBook + '}';
+    }
+    
+    
 }
+
