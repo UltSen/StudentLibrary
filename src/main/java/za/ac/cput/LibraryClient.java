dfa73b32b2a1a7ab949c9f1b985c852f29a296d6
CobainCupido <cobaincupido@gmail.com>
2022-10-23 17:27:04 +0200

    Set up the connection but the program says that sl does not exsist

--- src/main/java/za/ac/cput/LibraryClient.java
+++ src/main/java/za/ac/cput/LibraryClient.java
@@ -1,9 +1,38 @@
 package za.ac.cput;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.*;
+
 /*
 * StudentNo:217068324
 * Name: Cobain Liam Cupido
 * Class Function: 
 */
+
 public class LibraryClient {
-    
+    public static void main(String args[]) {
+        try {
+            Socket sl = new Socket("192.168.18.35", 5432);
+            System.out.println("Connection established at port 5432");
+
+            //InputStream is = new sl.getInputStream();
+
+           // DataInputStream dis = new DataInputStream(is);
+           // System.out.println("Getting Data");
+            
+            
+           // dis.close();
+            sl.close();
+            System.out.println("Connection Closed");
+        } catch (ConnectException connExcep) {
+            System.out.println("Error" + connExcep.getMessage());
+        } catch (IOException ioExcep) {
+            System.out.println("Error" + ioExcep.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
+    }
 }
